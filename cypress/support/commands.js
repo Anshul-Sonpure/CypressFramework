7f9e370e33f03263711cf3c0e08cca8f7157abc1
Anshul Sonpure <thealchemist20@protonmail.com>
2023-09-10 14:42:43 +0530

    First Commit

---
+++ cypress/support/commands.js
@@ -0,0 +1,40 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+/// <reference types="cypress"/>
+/// <reference types="cypress-xpath"/>
+
+cy.on('uncaught:exception', (err, runnable) => {
+    // Return false to prevent Cypress from failing the test
+    return false;
+})
+Cypress.Commands.add('logToExternalFile', (message) => {
+    // Define the log file path
+    const logFilePath = 'cypress/logs/application.log';
+  
+    // Create or append to the log file
+    cy.writeFile(logFilePath, `[${new Date().toISOString()}] ${message}\n`, { flag: 'a+' });
+});
