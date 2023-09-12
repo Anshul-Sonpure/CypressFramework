c43f6a2c45c91b64bc249221f6cb7c0b1616b469
Anshul Sonpure <thealchemist20@protonmail.com>
2023-09-12 22:36:40 +0530

    addded reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require('cypress');
+
+const reportDir = process.env.REPORT_DIR || 'cypress/reports';
+const reportName = process.env.REPORT_NAME || 'TestReport';
+
 module.exports = defineConfig({
-reporter: 'cypress-mochawesome-reporter',
-e2e: {
-setupNodeEvents(on, config) {
-require('cypress-mochawesome-reporter/plugin')(on);
-},
-},
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: `${reportDir}/${reportName}`, // Report directory based on environment variables
+    overwrite: false, // Set to true to overwrite reports on each run
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
 });
