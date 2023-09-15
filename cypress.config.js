f53cd518b7918adb216ac3169a2db3eb701e6f6f
Anshul Sonpure <thealchemist20@protonmail.com>
2023-09-15 14:40:02 +0530

    added mocha report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require('cypress');
-
+//For connecting to SQL Server
+const mysql = require('mysql')
 const reportDir = process.env.REPORT_DIR || 'cypress/reports';
 const reportName = process.env.REPORT_NAME || 'TestReport';
 
 module.exports = defineConfig({
+  projectId: "kveppv",
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: `${reportDir}/${reportName}`, // Report directory based on environment variables
@@ -14,4 +16,40 @@ module.exports = defineConfig({
       require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
+  "env": {
+    "db": {
+      "host": "db4free.net",
+      "user": "mobilestoreuser",
+      "password": "mobilestoreuser",
+      "database": "mymobilestore"
+    }
+  }
 });
+module.exports = (on, config) => {
+  // Usage: cy.task('queryDb', query)
+  on("task", {
+    queryDb: query => {
+      return queryTestDb(query, config);
+    }
+  });
+};
+
+
+
+
+function queryTestDb(query, config) {
+  // creates a new mysql connection using credentials from cypress.json env's
+  const connection = mysql.createConnection(config.env.db)
+  // start connection to db
+  connection.connect()
+  // exec query + disconnect to db as a Promise
+  return new Promise((resolve, reject) => {
+    connection.query(query, (error, results) => {
+      if (error) reject(error)
+      else {
+        connection.end()
+        return resolve(results)
+      }
+    })
+  })
+}
--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,54 +1,6 @@
-import '../support/commands';
-import { Faker, en } from '@faker-js/faker';
-
-const faker = new Faker({ locale: [en] });
-const apiconfig = require('../apiconfig.json');
-const { login, getproducts, getproductswithtoken, createProduct, updateProduct} = require('../support/apiUtils');
-const loginuser = require('../fixtures/loginuser.json');
-
-const productId = 8;
-const payload = {
-
-  cost: faker.number.int({ min: 100, max: 90000 }),
-  quantity: faker.number.int({ min: 100, max: 500 }),
-  locationId: faker.number.int({ min: 1, max: 5 }),
-  DealerId: faker.number.int({ min: 1, max: 5 })
-}
-var statusCode;
-describe('Update', () => {
-  it('Validate user is able to update existing products', () => {
-    login(`${loginuser.email}`, `${loginuser.password}`)
-      .then((result) => {
-        const accessToken = result.access_token; // Access the access_token
-        updateProduct(payload, accessToken, productId).then((response) => {
-          statusCode = response.status; // Set the status code to statusCode
-          expect(statusCode).to.equal(200); // Assert the status code here
-          expect(response.body.quantity).to.eq(payload.quantity);
-          cy.logger('apitest', 'Validate user is able to create new products -- Test Passed');
-          cy.logger('apitest', JSON.stringify(response.body, null, 2));
-        });
-      });
-  });
-
-  it('Second Test: Wait for Status Code 200', () => {
-   if(statusCode == 200)
-   {
-    cy.wait(2500)
-     // Now you can use 'statusCode' in your assertions or other tests
-     cy.request({
-      method: 'GET',
-      url: 'https://reqres.in/api/users?page=2',
-    }).then((response) => {
-      expect(statusCode).to.equal(200);
+describe('Database Query', () => {
+  it('should execute an SQL query', () => {
+    const query = 'SELECT * FROM user_details';
+    cy.task("queryDb",query);
     });
-  }
-})
-  it('Third Test: Another Test Block', () => {
-    cy.request({
-      method: 'GET',
-      url: 'https://reqres.in/api/users/2',
-    }).then((response) => {
-      expect(statusCode).to.equal(200);
-    });
-  });
 });
